c6c34ff55cf45ed4b8d06f1d9d0f78fd4cce4fdb
vladislav-i <saintmz3@gmail.com>
2015-05-04 17:00:02 -0700

    [feature] User is able to retrieve rooms and messages for that room.

--- client/app/communityFrame/chat/chat.js
+++ client/app/communityFrame/chat/chat.js
@@ -3,19 +3,23 @@ angular.module('interim.chat', ["firebase"])
 .controller('ChatController', function ($scope, $firebaseArray, $rootScope) {
 
   var ref = new Firebase("https://interim.firebaseio.com/room-messages");
-  // download the data into a local object
-
-  $scope.messagesTest = function(){
-
+  //get all messages for specific room
+  $scope.messagesTest = function() {
+    //get data for specific room
     var messages = $firebaseArray(ref.child($rootScope.roomId));
+    $scope.messages = messages;
     console.log(messages);
-
-      // console.log($rootScope.roomId);
-      $scope.messages = messages;
-      $scope.username = messages.name;
-
   }
 
-
+  //TODO: send messages to specific room
+  $scope.sendMessage = function() {
+    var message = {
+      userId: "anonymous:-JoWKcuh_THLgIR8wXKy",
+      name: "-JoWKcuh",
+      timestamp: Firebase.ServerValue.TIMESTAMP,
+      message: messageContent,
+      type: 'default'
+    }
+  }
 
 });
