e1e2eb08a37720d2010f58f84893773d29ca6407
Trace Thompson <tracethmpsn@gmail.com>
2015-05-05 14:06:57 -0700

    [feature] github OAuth works

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,20 +1,34 @@
 angular.module('interim.services', [])
 
-.factory('SomeName', function ($http) {
+.factory('Github', function ($q) {
   // Your code here
   
-  //example function
-  var getAll = function () {
-    return $http({
-      method: 'GET',
-      url: '/api/links'
+  //This sends the OAth request to github through firebase's 
+  //native functionalities and turns the results into a promise
+  var githubAuth = function () {
+    var deferred = $q.defer();
+    var ref = new Firebase("https://interim.firebaseio.com");
+    var githubAuth = ref.authWithOAuthPopup("github", function(error, authData) {
+      if (error) {
+        deferred.reject(error);
+      } else {
+        deferred.resolve(authData);
+      }
     })
-    .then(function (resp) {
-      return resp.data;
+    return deferred.promise;
+  }
+
+  //returns promise of authenticated user data to the controller
+  var firePromise = function(){
+    var githubPromisified = githubAuth();
+    return githubPromisified.then(function(auth) {
+      console.log("github user obj ",auth);
+    }, function(reason) {
+      return reason
     });
-  };
+  }
 
   return {
-    getAll: getAll
+    firePromise: firePromise
   }
 });
