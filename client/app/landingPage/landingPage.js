5873c6b5ba3d13d6d2cb258b4921ece5463b7191
Trace Thompson <tracethmpsn@gmail.com>
2015-05-06 10:24:56 -0700

    [cleanup] added comment to landing page function

--- client/app/landingPage/landingPage.js
+++ client/app/landingPage/landingPage.js
@@ -12,7 +12,10 @@ angular.module('interim.landingPage', [])
       $state.go('community');
 
 
-      //if the user is an admin grant permissions to rare super admin page
+      //if the user is an admin grant permissions to ~very~ rare super admin page
+      //we actually wanna make this a factory function and refactor
+      //the function to actually query the database and check if the
+      //user has admin priveledges 
       if(userObj.github.displayName === "Trace Thompson"){
         $rootScope.permission = true
       }
