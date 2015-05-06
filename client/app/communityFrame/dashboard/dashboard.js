7daafc58e641fbdba61f80ba94db5728bcaff0aa
vladislav-i <saintmz3@gmail.com>
2015-05-06 10:24:12 -0700

    [refactor] Dashboard to add rooms, asses bootbox.js

--- client/app/communityFrame/dashboard/dashboard.js
+++ client/app/communityFrame/dashboard/dashboard.js
@@ -3,27 +3,67 @@ angular.module('interim.dashboard', ["firebase"])
 .controller('DashboardController', function($scope, $firebaseArray, $rootScope) {
 
   var roomRef = new Firebase("https://interim.firebaseio.com/room-metadata");
+  var privateRef = new Firebase("https://interim.firebaseio.com/private-metadata");
   // download the data into a local object
   $scope.rooms = $firebaseArray(roomRef);
 
+  // var privateRooms = $firebaseArray(privateRef.child($rootScope.user));
+  // privateRooms.$add({name: "privateTest"});
+
+  // var newRoom = privateRef.push();
+
+  // privateRoom = {
+  //     id: newRoom.key(),
+  //     //TODO: have to fix created by userid  when user is logged in
+  //     createdByUserId: "anonymous:-JoQq9FpU-oOGmI7E4Mc",
+  //     name: 'SecretRoom',
+  //     type: 'private',
+  //     createdAt: Firebase.ServerValue.TIMESTAMP
+  //   };
+
+  // newRoom.set(privateRoom);
+
+  // $scope.privateRooms = $firebaseArray(privateRef);
+
+
+
+        //$scope.rooms.concat(privateRooms);
+      //}
+  
+  //console.log("rooms: ", $scope.rooms);
+
   //adding room
   $scope.add = function(event) {
-    //sweetprompt use it here
-    var roomName =  prompt("Please enter new room name?");
-    //creates entry
-    var newRoom = roomRef.push();
-    //create new room
-    var room = {
-      id: newRoom.key(),
-      //TODO: have to fix created by userid  when user is logged in
-      createdByUserId: "anonymous:-JoQq9FpU-oOGmI7E4Mc",
-      name: roomName,
-      type: 'public',
-      createdAt: Firebase.ServerValue.TIMESTAMP
-    };
-    //sets data
-    newRoom.set(room, function(error) {
-        // room successfully created
+   //getting user room name here;
+    var roomName;
+
+    bootbox.dialog({
+      message: "Enter New Room Name:<input type='text' id='room_name'></input>",
+      title: "Creating New Room",
+      buttons: {
+        main: {
+          label: "Create",
+          className: "btn-primary",
+          callback: function() {
+            roomName = $('#room_name').val();
+            //creates entry
+            var newRoom = roomRef.push();
+            //create new room
+            var room = {
+              id: newRoom.key(),
+              //TODO: have to fix created by userid when user is logged in
+              createdByUserId: "anonymous:-JoQq9FpU-oOGmI7E4Mc",
+              name: roomName,
+              type: 'public',
+              createdAt: Firebase.ServerValue.TIMESTAMP
+            };
+            //sets data
+            newRoom.set(room, function(error) {
+                // room successfully created
+            });
+          }
+        }
+      }
     });
   }
 
