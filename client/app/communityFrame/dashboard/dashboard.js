7a7c5539a57367e1bde719c908f4bc187892e33a
vladislav-i <saintmz3@gmail.com>
2015-05-06 15:30:21 -0700

    [feature] Created private room selection

--- client/app/communityFrame/dashboard/dashboard.js
+++ client/app/communityFrame/dashboard/dashboard.js
@@ -3,42 +3,21 @@ angular.module('interim.dashboard', ["firebase"])
 .controller('DashboardController', function($scope, $firebaseArray, $rootScope) {
 
   var roomRef = new Firebase("https://interim.firebaseio.com/room-metadata");
-  var privateRef = new Firebase("https://interim.firebaseio.com/private-metadata");
-  // download the data into a local object
-  $scope.rooms = $firebaseArray(roomRef);
+  //display room names
+  var publicRooms = $firebaseArray(roomRef);
+  // $scope.rooms = $firebaseArray(roomRef);
+  $scope.rooms = publicRooms;
 
-  // var privateRooms = $firebaseArray(privateRef.child($rootScope.user));
-  // privateRooms.$add({name: "privateTest"});
-
-  // var newRoom = privateRef.push();
-
-  // privateRoom = {
-  //     id: newRoom.key(),
-  //     //TODO: have to fix created by userid  when user is logged in
-  //     createdByUserId: "anonymous:-JoQq9FpU-oOGmI7E4Mc",
-  //     name: 'SecretRoom',
-  //     type: 'private',
-  //     createdAt: Firebase.ServerValue.TIMESTAMP
-  //   };
-
-  // newRoom.set(privateRoom);
-
-  // $scope.privateRooms = $firebaseArray(privateRef);
-
-
-
-        //$scope.rooms.concat(privateRooms);
-      //}
-  
-  //console.log("rooms: ", $scope.rooms);
 
   //adding room
   $scope.add = function(event) {
    //getting user room name here;
-    var roomName;
-
+    var roomName, roomType = 'public';
+    
     bootbox.dialog({
-      message: "Enter New Room Name:<input type='text' id='room_name'></input>",
+      // input box for room name and set room to public
+      message: "Enter New Room Name: <input type='text' id='room_name'></input>" + 
+      "<div><label><input type='checkbox' id = 'checkbox'> Private Room</label></div>",
       title: "Creating New Room",
       buttons: {
         main: {
@@ -46,6 +25,10 @@ angular.module('interim.dashboard', ["firebase"])
           className: "btn-primary",
           callback: function() {
             roomName = $('#room_name').val();
+            //if checked then set room to private
+            if($("#checkbox").is(':checked') === true) {
+              roomType = 'private';
+            }
             //creates entry
             var newRoom = roomRef.push();
             //create new room
@@ -54,7 +37,7 @@ angular.module('interim.dashboard', ["firebase"])
               //TODO: have to fix created by userid when user is logged in
               createdByUserId: "anonymous:-JoQq9FpU-oOGmI7E4Mc",
               name: roomName,
-              type: 'public',
+              type: roomType,
               createdAt: Firebase.ServerValue.TIMESTAMP
             };
             //sets data
@@ -77,7 +60,6 @@ angular.module('interim.dashboard', ["firebase"])
   // var userRef = new Firebase("https://interim.firebaseio.com/user-names-online");
   // $scope.users = $firebaseArray(userRef);
 
-  //get all messages for specific room
   var userRef = new Firebase("https://interim.firebaseio.com/UsersDB");
   $scope.users = $firebaseArray(userRef);
 
