ae5e714492b7d569dd1bf466b835e99e8a64d875
Thomas Logan <thomaslogan@Thomass-MacBook-Pro.local>
2015-05-06 15:16:55 -0700

    [feat] Added user profile popups to the profile pictures in the chat room.

--- client/app/userProfile/userProfile.js
+++ client/app/userProfile/userProfile.js
@@ -1,6 +1,10 @@
 angular.module('interim.userProfile', [])
 
-.controller('UserProfileController', function ($scope, factoryPlaceHolder) {
+.controller('UserProfileController', function ($scope, $firebaseArray, $modal, $log) {
   // Your code here
+  userInfo = new Firebase("https://interim.firebaseio.com/UsersDB");
   
+  $scope.users = $firebaseArray(userInfo);
+  console.log($scope.users);
+
 });
