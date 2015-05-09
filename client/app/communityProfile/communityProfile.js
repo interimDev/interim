213029dc80b499e5821187092dcf212ca32bab29
Trace Thompson <tracethmpsn@gmail.com>
2015-05-09 09:56:15 -0700

    [cleanup] fix console log in comm profile

--- client/app/communityProfile/communityProfile.js
+++ client/app/communityProfile/communityProfile.js
@@ -2,6 +2,6 @@ angular.module('interim.communityProfile', [])
 
 .controller('CommunityProfileController', function ($scope, $firebaseArray, $rootScope) {
   // Your code here
-  console.log("ugh comm obj: ",$rootScope.communityInfo)
+  console.log("community obj: ",$rootScope.communityInfo)
   $scope.community = $rootScope.communityInfo
 });
