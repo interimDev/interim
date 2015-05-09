fa956dca68be80ec864b3f410431e9de33bb84a2
Thomas Logan <thomaslogan@Thomass-MacBook-Pro.local>
2015-05-09 16:05:18 -0700

    [fix] working out the location of the edit icon.

--- client/app/communityFrame/userBottomSidebar/userBottomSidebar.js
+++ client/app/communityFrame/userBottomSidebar/userBottomSidebar.js
@@ -3,6 +3,7 @@ angular.module('interim.userBottomSidebar', ["firebase"])
 .controller('BottomSidebarController', function ($scope, $rootScope) {
   //getting users current name from github
  $scope.userName = $rootScope.userInfo.name;
+ console.log($scope.userName);
  //getting users current image from github
  $scope.profileImage = $rootScope.userInfo.avi_url;
   
