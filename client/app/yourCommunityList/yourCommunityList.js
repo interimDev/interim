eac13dc6fd2aada7e01544500c200ae8f45b9ad4
Janessa Link <janessa@gmail.com>
2015-05-09 13:51:04 -0700

    [feature] Communties selection page with basic display  - User information recieved and saved to $scope from auth  - Database calls to save user's users and groups to $scope

--- client/app/yourCommunityList/yourCommunityList.js
+++ client/app/yourCommunityList/yourCommunityList.js
@@ -1,6 +1,66 @@
-angular.module('interim.yourCommunityList', [])
+angular.module('interim.yourCommunityList', ["firebase"])
 
-.controller('YourCommunityListController', function ($scope, factoryPlaceHolder) {
-  // Your code here
-  
+.controller('YourCommunityListController', function ($scope, $firebaseArray, $rootScope) {
+  // Initially identifying user and displaying their current groups & communities
+
+  var ref = new Firebase("https://interim.firebaseio.com/");
+  $scope.userInfo = $rootScope.userInfo;
+
+  // For each of these calls, userId needs to be in the form
+  // userName-authSource   // Yoda-github
+  $scope.usersCommunities = function() {
+
+    var userId = $scope.userInfo.name + "-" + $scope.userInfo.auth.provider;
+
+    var communities = $firebaseArray(ref.child('UsersDB').child(userId).child('usersCommunities'));
+    console.log("Retrieved ", $scope.userInfo.name, "'s communities: ", communities);
+    $scope.communities = communities;
+
+  }
+
+  $scope.usersGroups = function(userId) {
+    //Check all Group children for all communities
+    var groups = $firebaseArray(ref.child('UsersDB').child(userId).child('usersGroups'));
+    $scope.groups = groups;
+
+  }
+
+
+
+  $scope.cacheCommunities = function(userId) {
+    //AngularFire query to DB
+    //Store in $rootscope array
+
+
+  };
+  $scope.cacheGroups = function(userId) {
+    //AngularFire query to DB
+    //Store in $rootscope array
+  };
+  $scope.displayUsersCommunities= function(){
+    //Use $rootscope array of communties
+
+  };
+  $scope.displayUsersGroups= function(){
+    //Use $rootscope array of communties
+  };
+
+
+  // Seach and display results
+  $scope.searchCommunities = function(searchTerm) {
+  };
+  $scope.searchGroups = function(searchTerm) {
+  };
+  $scope.displayResults = function(searchTerm) {
+  };
+
+  // Selecting groups or communities after search results to request permission
+
+  $scope.selectCommunities = function(){
+  };
+  $scope.selectGroups = function(){
+  };
+
+
+
 });
