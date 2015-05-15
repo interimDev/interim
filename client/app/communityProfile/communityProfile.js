bea436d0bc94a0dad2b3b630032b26f9d02cad88
Trace Thompson <tracethmpsn@gmail.com>
2015-05-15 11:52:08 -0700

    [refactor] fixes bugs caused by cleanup

--- client/app/communityProfile/communityProfile.js
+++ client/app/communityProfile/communityProfile.js
@@ -3,7 +3,7 @@ angular.module('interim.communityProfile', [])
 .controller('CommunityProfileController', function ($scope, $firebaseArray, $rootScope, $state, $firebaseObject, community, $modal, Auth) {
   var userCurrentID = $rootScope.userInfo ? $rootScope.userInfo.id : $rootScope.communityInfo.id;
   var communityGroupsRef = new Firebase("https://interim.firebaseio.com/community-groups-metadata");
-  var groups = $firebaseArray(communityGroupsRef)
+  var groups = $firebaseArray(communityGroupsRef);
 
   $scope.groups = groups;
   $scope.community = community;
@@ -43,15 +43,22 @@ angular.module('interim.communityProfile', [])
 
   //filter public groups for community
   $scope.publicGroup = function(group) {
-    var communityName = $rootScope.communityInfo ? $rootScope.communityInfo.name : $scope.community.name;
-    if (group.type === "public" && group.community === communityName) {
+    if (group.type === "public" && group.community === community.name) {
       return true;  
     }
   };
 
+  //get each group
+  $scope.getGroup = function(group) {
+    //setting group in rootscope
+    $rootScope.group = group;
+    //sending user to profile page
+    $state.go('community');
+  };
+
   //filter private groups for community
   $scope.privateGroup = function(group) {
-    if (group.type === "private" && group.community === communityName) {
+    if (group.type === "private" && group.community === community.name) {
       for (var val in group) {
         for (var id in group[val]) {
           //if users members match current users id then allow user to see room
@@ -66,7 +73,7 @@ angular.module('interim.communityProfile', [])
   
   //setting private group when user clicks on adding users
   $scope.selectingGroup = function(groupID) {
-    var usersGroup = groupID;
+    $scope.usersGroup = groupID;
   };
 
   //adding user to private group
@@ -74,16 +81,9 @@ angular.module('interim.communityProfile', [])
     var newUsers = {};
     var selectedGroup = new Firebase("https://interim.firebaseio.com/community-groups-metadata");
     newUsers[user.id]= user.id;
-    selectedGroup.child(usersGroup).child("usersList").update(newUsers);
+    selectedGroup.child($scope.usersGroup).child("usersList").update(newUsers);
     $.notify(user.name +" is added to group", "success");
   };
 
-  //get each group
-  $scope.getGroup = function(group) {
-    //setting group in rootscope
-    $rootScope.group = group;
-    //sending user to profile page
-    $state.go('community');
-  };
 
 });
